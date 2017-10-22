6380db5e175ad924b249b77a414b937155c39122
Silas Guimarães Ribeiro <guimaraes.silas@hotmail.com>
2017-10-22 16:59:40 -0200

    make it better

--- server/startup.js
+++ server/startup.js
@@ -3,6 +3,4 @@ Meteor.startup(() => {
     Meteor.publish("tarefas", function(){
         return Tarefas.find({usuario: this.userId});
     });
-   
-    process.env.MAIL_URL="smtp://guimaraes.silas2013%40gmail.com:minhasenha@smtp.gmail.com:587";
 });
