4f38a1e1bb001ac33f4a5cd65ad44f5e4c3a1dcd
Silas Guimarães Ribeiro <guimaraes.silas@hotmail.com>
2017-10-22 16:57:14 -0200

    make it better

--- server/startup.js
+++ server/startup.js
@@ -1,13 +1,8 @@
-import { Meteor } from 'meteor/meteor';
-
-if (Meteor.isServer){
-    Meteor.startup(() => {
+Meteor.startup(() => {
    
-        Meteor.publish("tarefas", function(){
-            return Tarefas.find({usuario: this.userId});
-        });
-   
-        process.env.MAIL_URL="smtp://guimaraes.silas2013%40gmail.com:minhasenha@smtp.gmail.com:465/";
+    Meteor.publish("tarefas", function(){
+        return Tarefas.find({usuario: this.userId});
     });
-}
-
+   
+    process.env.MAIL_URL="smtp://guimaraes.silas2013%40gmail.com:minhasenha@smtp.gmail.com:587";
+});
