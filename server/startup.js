9f3a3c0fe3b86b295d8bce2f6801f342042815ac
Silas Guimarães Ribeiro <guimaraes.silas@hotmail.com>
2017-10-22 16:12:27 -0200

    ROOT_URL

--- server/startup.js
+++ server/startup.js
@@ -1,4 +1,6 @@
 Meteor.startup(function(){
+
+    process.env.ROOT_URL = 'https://fasttasks.herokuapp.com:3000';    
     Meteor.publish("tarefas", function(){
         return Tarefas.find({usuario: this.userId});
     });
